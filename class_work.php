76898cd0a6928fc82700870bbd053219448aecd7
GTSA - Infinity <veljko2009@live.se>
2021-11-04 21:11:49 +0100

    Small fixes

--- class_work.php
+++ class_work.php
@@ -2,27 +2,21 @@
 
 class Work extends Thread {
 	private $row;
-	private $db;
 
 	public function __construct($row) {
 		$this->row = $row;
 		printf("A new work was submitted with the name: %s\n", $row['url']);
-		$this->db = $this->getDb();
 	}
 
 	public function run() {
 		$this->callUrl($this->row);
 	}
 
-	private function getDb() {
-		$db = new PDO("sqlite:".__DIR__."/mydb.db");
-		return $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}
-
 	private function callUrl($row) {
 		$this->updateStatusAndHttpCode($row['id'], 'PROCESSING');
 		if ( ! isset($row['url']) || ! $row['url'] || ! is_string($row['url']) || ! preg_match('/^http(s)?:\/\/[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(\/.*)?$/i', $row['url'])) {
 			$this->updateStatusAndHttpCode($row['id'], 'ERROR');
+			return;
 		}
 
 		$ch = curl_init($row['url']);
@@ -33,13 +27,18 @@ class Work extends Thread {
 		$output = curl_exec($ch);
 		$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 		curl_close($ch);
+
+		//If the returned http code is for example 500 should the status be ERROR?
 		$this->updateStatusAndHttpCode( $row['id'], 'DONE', $httpCode);
 	}
 
 	private function updateStatusAndHttpCode($jobId, $status, $httpCode = null) {
-		$sqlError = 'UPDATE called_urls SET status = :status, http_code = :httpCode  WHERE id = :id';
+		$db = new PDO("sqlite:".__DIR__."/mydb.db");
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-		$statement = $this->db->prepare($sqlError);
+		$updateSql = 'UPDATE called_urls SET status = :status, http_code = :httpCode  WHERE id = :id';
+
+		$statement = $db->prepare($updateSql);
 		$statement->execute([':status' => $status, ':id' => $jobId, ':httpCode' => $httpCode]);
 	}
 }
